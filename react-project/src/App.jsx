445bb937d7f49eee20047f9d3e03a1d17f7f96ec
Lakshman kumar <lakshmankumar2603@gmail.com>
2023-07-29 15:29:03 +0530

    change text with click on button

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import Card from './components/Card';
 import MyComponent from './components/myComponent';
 function App() {
-
+let [details, setDetails] = useState("submit test");
+const handleClick = () =>{
+  details = setDetails('Test submitted');
+}
   return (
     <>
       <h1>Test1</h1>
-      <h2>Test2</h2>
+      <h2>{details}</h2>
+      <button onClick={handleClick}>click me</button>
       <MyComponent />
       <Card name="Laksh" email="l@pw.live"/>
       <Card name="Lakshman" email="la@pw.live"/>
