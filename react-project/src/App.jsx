ee295f7a8f9f54053d80544d6310ac63cf60cca9
Lakshman kumar <lakshmankumar2603@gmail.com>
2023-07-29 14:57:20 +0530

    component added

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,10 +1,11 @@
-
+import MyComponent from './components/myComponent';
 function App() {
 
   return (
     <>
       <h1>Test1</h1>
       <h2>Test2</h2>
+      <MyComponent />
     </>
   )
 }
---
+++ react-project/src/components/myComponent.jsx
@@ -0,0 +1,14 @@
+// to get boiler platte -
+// rfce - react functional export component
+import React from 'react'
+
+function MyComponent() {
+  return (
+    <div>
+        <h1>1st component</h1>
+        <h2>2nd component</h2>
+    </div>
+  )
+}
+
+export default MyComponent
