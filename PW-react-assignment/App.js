8fe7f8327c213f92d94ddf7aed8568d43d01abae
Lakshman kumar <lakshmankumar2603@gmail.com>
2023-07-30 16:05:35 +0530

    List component added

--- PW-react-assignment/App.js
+++ PW-react-assignment/App.js
@@ -1,13 +1,18 @@
 import Person from "./components/Person";
 import Button from "./components/Button";
 import Header from "./components/Header";
+import List from "./components/List";
 
 App = () =>{
     return(
         <>
             <Person name="Lakshman" age="19" />
+
             <Button text="click me" onClick={()=>console.log('clicked')} />
+
             <Header title="This is Header"/>
+
+            <List items={['Iron-man', 'Thor', 'Wanda']} />
         </>
     )
 }
