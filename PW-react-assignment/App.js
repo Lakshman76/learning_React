118200ea7aa8b76420469090814f509b4c3614b7
Lakshman kumar <lakshmankumar2603@gmail.com>
2023-07-30 15:54:42 +0530

    Header component added

--- PW-react-assignment/App.js
+++ PW-react-assignment/App.js
@@ -1,11 +1,13 @@
 import Person from "./components/Person";
 import Button from "./components/Button";
+import Header from "./components/Header";
 
 App = () =>{
     return(
         <>
             <Person name="Lakshman" age="19" />
             <Button text="click me" onClick={()=>console.log('clicked')} />
+            <Header title="This is Header"/>
         </>
     )
 }
